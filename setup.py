e06e19b97c1a9ef6b5a6e0bbdb00f91597cc75ae
André Artelt <aartelt@techfak.de>
2026-06-24 10:15:49 +0200

    Build: Fix missing NumPy include

--- setup.py
+++ setup.py
@@ -1,3 +1,16 @@
 from setuptools import setup
+from setuptools.command.build_ext import build_ext
 
-setup()
+
+class build_ext_with_numpy(build_ext):
+    def build_extensions(self):
+        import numpy
+
+        numpy_include = numpy.get_include()
+        for ext in self.extensions:
+            ext.include_dirs.append(numpy_include)
+
+        super().build_extensions()
+
+
+setup(cmdclass={"build_ext": build_ext_with_numpy})
